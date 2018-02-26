8f36ed06095ba105ad3a3e3c1af68e3ac21c7d4b
oconnill <doconnelldesign@gmail.com>
2018-02-26 15:04:07 -0700

    delete message working

--- routes/messages.js
+++ routes/messages.js
@@ -5,30 +5,30 @@ var Message = require('../models/message');
 
 router.get('/', function (req, res, next) {
     Message.find()
-    .exec(function(err, messages){
-        if (err) {
-            return res.status(500).json({
-                title: 'Error Alert',
-                error: err
-            });
-        }
-        res.status(200).json({
-            message: 'Success',
-            obj: messages
-        })
+        .exec(function (err, messages) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'Error Alert',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: messages
+            })
 
-    })
+        })
 })
 
 router.post('/', function (req, res, next) {
-    var message = new Message ({
-       content: req.body.content 
+    var message = new Message({
+        content: req.body.content
     });
-    message.save(function(err, result){
-        if(err){
+    message.save(function (err, result) {
+        if (err) {
             res.status(500).json({
                 title: 'Error',
-                error: err 
+                error: err
             });
         }
         res.status(201).json({
@@ -38,9 +38,9 @@ router.post('/', function (req, res, next) {
     })
 });
 
-router.patch('/:id', function(req,res,next){
-    Message.findById(req.params.id, function(err, message) {
-        if (err){
+router.patch('/:id', function (req, res, next) {
+    Message.findById(req.params.id, function (err, message) {
+        if (err) {
             return res.status(500).json({
                 title: 'Error Alert',
                 error: err
@@ -49,24 +49,55 @@ router.patch('/:id', function(req,res,next){
         if (!message) {
             return res.status(500).json({
                 title: 'No Message',
-                error: {message: 'Message not found'}
+                error: { message: 'Message not found' }
             })
         }
         message.content = req.body.content;
-        message.save(function(err, result){
-            if(err){
+        message.save(function (err, result) {
+            if (err) {
                 return res.status(500).json({
                     title: 'Error',
-                    error: err 
+                    error: err
                 });
             }
             res.status(200).json({
                 message: 'Saved Mesage to DB',
                 obj: result
-            })  
+            })
+        })
     })
-})
 
 })
+
+router.delete('/:id', function(req, res, next) {
+    Message.findById(req.params.id, function (err, message) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        if (!message) {
+            return res.status(500).json({
+                title: 'No Message Found!',
+                error: {message: 'Message not found'}
+            });
+        }
+        message.remove(function(err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Deleted message',
+                obj: result
+            });
+        });
+    });
+});
+
+
 
 module.exports = router;
