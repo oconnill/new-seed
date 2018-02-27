1418ce2ca0dd8da2ecf87fdb3d2e5f66a942bcfd
oconnill <doconnelldesign@gmail.com>
2018-02-27 10:03:15 -0700

    sign in and sign out

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -18,7 +18,7 @@ export class MessageService {
         return this.http.post('http://localhost:3000/message', body, {headers: headers})
             .map((response: Response) => {
                 const result = response.json();
-                const message = new Message(result.obj.content, 'Dummy', result.obj._id, null)
+                const message = new Message(result.obj.content, 'Dummy', result.obj._id, null);
                 this.messages.push(message);
                 return message;
             })
--- routes/messages.js
+++ routes/messages.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken')
+
+var User = require('../models/user')
 
 var Message = require('../models/message');
 
@@ -20,22 +23,48 @@ router.get('/', function (req, res, next) {
         })
 })
 
-router.post('/', function (req, res, next) {
-    var message = new Message({
-        content: req.body.content
-    });
-    message.save(function (err, result) {
+router.use('/', function (req, res, next) {
+    jwt.verify(req.query.token, 'secret', function (err, decoded) {
         if (err) {
-            res.status(500).json({
-                title: 'Error',
+            return res.status(401).json({
+                title: 'Not Authorized',
                 error: err
             });
         }
-        res.status(201).json({
-            message: 'Saved Mesage to DB',
-            obj: result
-        })
+        next();
     })
+});
+
+router.post('/', function (req, res, next) {
+    //get user from token
+    var decoded = jwt.decode(req.query.token);
+    User.findById(decoded.user._id, function (err, result) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occured',
+                error: err
+            });
+        }
+        var message = new Message({
+            content: req.body.content,
+            user: user
+        });
+        message.save(function (err, result) {
+            if (err) {
+                res.status(500).json({
+                    title: 'Error',
+                    error: err
+                });
+            }
+            user.messages.push(result);
+            user.save();
+            res.status(201).json({
+                message: 'Saved Mesage to DB',
+                obj: result
+            });
+        });
+    });
+
 });
 
 router.patch('/:id', function (req, res, next) {
@@ -69,34 +98,35 @@ router.patch('/:id', function (req, res, next) {
 
 })
 
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', function (req, res, next) {
     Message.findById(req.params.id, function (err, message) {
         if (err) {
             return res.status(500).json({
-                title: 'An error occurred',
+                title: 'Error Alert',
                 error: err
             });
         }
         if (!message) {
             return res.status(500).json({
-                title: 'No Message Found!',
-                error: {message: 'Message not found'}
-            });
+                title: 'No Message',
+                error: { message: 'Message not found' }
+            })
         }
-        message.remove(function(err, result) {
+        message.content = req.body.content;
+        message.remove(function (err, result) {
             if (err) {
                 return res.status(500).json({
-                    title: 'An error occurred',
+                    title: 'Error YOOOOO',
                     error: err
                 });
             }
             res.status(200).json({
-                message: 'Deleted message',
+                message: 'Deleted Message from DB',
                 obj: result
-            });
-        });
-    });
-});
+            })
+        })
+    })
+})
 
 
 
